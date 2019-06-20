02fba0d86f65ea7e6fc377b8c9fed6a13de5af7c
Lucas Pires <luc_50@outlook.com>
2019-06-20 20:20:23 -0300

    1.2 Commit Ajuste no cálculo.

--- NetBeansProjects/JurosSimples/src/jurossimples/JurosSimples.java
+++ NetBeansProjects/JurosSimples/src/jurossimples/JurosSimples.java
@@ -12,7 +12,7 @@ public class JurosSimples {
         double juros;
         double periodoAnalisado ;
         double montante;
-        double porcentagem;
+        double porcentagem = 0;
         String aux = "";
 
         while (true) {
@@ -34,7 +34,11 @@ public class JurosSimples {
                     juros = leia.nextDouble();
                     System.out.println("Informe o Valor do Período a ser Analisado em meses: ");
                     periodoAnalisado = leia.nextDouble();
-                    porcentagem = juros / 100;
+                    if(juros > 1){
+                        porcentagem = juros / 100;
+                    }else{
+                        porcentagem = juros;
+                    }
                     montante = capital * (1 + porcentagem * periodoAnalisado );
                     System.out.println("Montante: " + "R$" + montante);
                     break;
@@ -46,19 +50,23 @@ public class JurosSimples {
                     juros = leia.nextDouble();
                     System.out.println("Informe o Valor do Montante: ");
                     montante = leia.nextDouble();
-                    porcentagem = juros / 100;
-                    periodoAnalisado = ((montante / capital) - 1) / (porcentagem);
+                    if(juros > 1){
+                        porcentagem = juros / 100;
+                    }else{
+                        porcentagem = juros;
+                    }
+                    periodoAnalisado = ((montante - capital) / (capital * porcentagem));
                     System.out.println("Tempo necessário para investimento: " + periodoAnalisado + " Meses.");
                     break;
                 case 'j':  //JUROS
-                    System.out.println("Escolheu a opção para calcular o juros.");
+                    System.out.println("Escolheu a opção para calcular a taxa de juros.");
                     System.out.println("Informe o Valor do Capital: ");
                     capital = leia.nextDouble();
                     System.out.println("Informe o Valor do Montante: ");
                     montante = leia.nextDouble();
                     System.out.println("Informe o Valor do Período a ser Analisado em meses: ");
                     periodoAnalisado = leia.nextDouble();
-                    juros = ((montante / capital) - 1 / periodoAnalisado ) * 100;
+                    juros = ((montante - capital) / (capital * periodoAnalisado) );
                     System.out.println("Juros de : " + juros + "%");
                     break;
                 case 'c': //CAPITAL
